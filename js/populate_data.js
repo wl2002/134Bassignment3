72f5b88e1661ab6bc1c554d90b3fbade63a1477c
noor bakkal <noor_bakkal@hotmail.com>
2015-07-26 14:52:53 -0700

    populate data is connected now with some test database file
    populate data is connected to some test database file please review the
    files that i add to help u code for other pages or text me thanks

--- js/populate_data.js
+++ js/populate_data.js
@@ -1,4 +1,16 @@
-function addRow(tagName,tagDes)
+function addRow(){
+var scoresRef = new Firebase("https://dinosaur-facts.firebaseio.com/scores");
+scoresRef.orderByValue().on("value", function(snapshot) {
+  snapshot.forEach(function(data) {
+    console.log("The " + data.key() + " dinosaur's score is " + data.val());
+    addRow1(data.key(),data.val());
+  
+  });
+});
+
+}
+
+function addRow1(tagName,TagDes)
 {
 	if (!document.getElementsByTagName) return;
 	tabBody=document.getElementsByTagName("tbody").item(0);
@@ -7,7 +19,7 @@ function addRow(tagName,tagDes)
 	cell2 = document.createElement("td");
 
 	textnode1=document.createTextNode(tagName);
-	textnode2=document.createTextNode(tagDes);
+	textnode2=document.createTextNode(TagDes);
 	
 	cell1.appendChild(textnode1);
 	cell2.appendChild(textnode2);
